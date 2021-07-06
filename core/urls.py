910c944aef39c46832312b39a08f356795aef17d
KadhaDeemi <dami.valderrama@duocuc.cl>
2021-07-05 21:20:15 -0400

    Se implementa el crud eliminar se crea crea y adorna nuevos html

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, form_proveedor, Ver, form_modificar
+from .views import home, form_proveedor, Ver, form_modificar,form_eliminar
 
 urlpatterns =[
     path('', home, name="home"),
     path('Form_creaproveedor', form_proveedor, name="crear"),
     path('Ver', Ver, name="ver"),
-    path('Form_modProveedor/<id>/', form_modificar, name="Modificar")
+    path('Form_modProveedor/<id>/', form_modificar, name="Modificar"),
+    path('form_eliminar/<id>/', form_eliminar, name="Eliminar")
 ]
--- core/views.py
+++ core/views.py
@@ -28,13 +28,13 @@ def form_modificar(request,id):
         'form': proveedorForm(instance=proveedor)
     }
     if request.method == 'POST': 
-        formulario = proveedorForm(data=request.POST, instance = proveedor)
+        formulario = proveedorForm(data=request.POST, instance = proveedor, files=request.FILES)
         if formulario.is_valid: 
             formulario.save()
             return redirect('ver')
     return render(request, 'core/Form_modProveedor.html', datos)
 
 def form_eliminar(request,id):
-    proveedor = Proveedor.objects.get(nroChip=id)
+    proveedor = Proveedor.objects.get(numIdentificacion=id)
     proveedor.delete()
     return redirect('ver')
