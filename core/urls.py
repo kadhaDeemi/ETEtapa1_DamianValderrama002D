28270c77105225eba13571877f6e657d35ad1bda
KadhaDeemi <dami.valderrama@duocuc.cl>
2021-07-05 23:59:33 -0400

    se crea api con los necesario en views.py, urls.py y se adorna pagina web

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import home, form_proveedor, Ver, form_modificar,form_eliminar
+from .views import home, form_proveedor, Ver, form_modificar,form_eliminar,lista_proveedores, lista_api, detalle_proveedor
+
 
 urlpatterns =[
     path('', home, name="home"),
     path('Form_creaproveedor', form_proveedor, name="crear"),
     path('Ver', Ver, name="ver"),
     path('Form_modProveedor/<id>/', form_modificar, name="Modificar"),
-    path('form_eliminar/<id>/', form_eliminar, name="Eliminar")
+    path('form_eliminar/<id>/', form_eliminar, name="Eliminar"),
+    path('lista_proveedores', lista_proveedores, name="lista_proveedores"),
+    path('lista_api', lista_api, name="lista_api"),
+    path('detalle_proveedor/<id>', detalle_proveedor, name="detalle_proveedor")
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, redirect
 from .models import Proveedor
 from .forms import proveedorForm
+from django.views.decorators import csrf
+from rest_framework.serializers import Serializer
+from .serializers import ProveedorSerializer
+from rest_framework import status 
+from rest_framework.decorators import api_view, permission_classes 
+from rest_framework.response import Response 
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def home(request):
@@ -38,3 +47,53 @@ def form_eliminar(request,id):
     proveedor = Proveedor.objects.get(numIdentificacion=id)
     proveedor.delete()
     return redirect('ver')
+
+
+
+'''Serializers '''
+
+
+@csrf_exempt
+@api_view(['GET', 'POST'])
+
+
+def lista_proveedores(request):
+    if request.method== 'GET':
+        prove = Proveedor.objects.all()
+        serializer = ProveedorSerializer(prove, many=True)
+        return Response(serializer.data)
+
+    elif request.method=='POST': 
+        data = JSONParser().parse(request)
+        serializer = ProveedorSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else: 
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def lista_api(request):
+    return render(request, 'ApiWeb.html')
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes((IsAuthenticated,))
+def detalle_proveedor(request,id): 
+    try: 
+        proveedor = Proveedor.objects.get(numIdentificacion=id) 
+    except Proveedor.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method=='GET': 
+        serializer = ProveedorSerializer(vehiculo)
+        return Response(serializer.data)
+    if request.method=='PUT': 
+        data = JSONParser().parse(request)
+        serializer = ProveedorSerializer(vehiculo, data = data)
+        if serializer.is_valid(): 
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response (serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+    elif request.method=='DELETE': 
+        proveedor.delete()
+        return Response (status = status.HTTP_204_NO_CONTENT)
